b2b2ad19a5d3e072564365fc57d773de73ec4498
CosminGGeorgescu <cosmingg2013@gmail.com>
2023-12-02 21:40:07 +0200

    2023/2 done

--- year2023/src/day2.rs
+++ year2023/src/day2.rs
@@ -1,3 +1,5 @@
+use std::collections::HashMap;
+
 use crate::input;
 
 fn possible_games() -> Result<usize, std::io::Error> {
@@ -14,6 +16,7 @@ fn possible_games() -> Result<usize, std::io::Error> {
                 .unwrap()
                 .split(&[';', ' ', ','])
                 .filter(|&x| x != "");
+
             if true
                 && loop {
                     if let Some(nr) = sets.next() {
@@ -37,7 +40,42 @@ fn possible_games() -> Result<usize, std::io::Error> {
         .sum())
 }
 
+fn min_set_power_sum() -> Result<usize, std::io::Error> {
+    Ok(std::fs::read_to_string(input::DAY2)?
+        .trim_end()
+        .split('\n')
+        .collect::<Vec<&str>>()
+        .iter()
+        .map(|&game| {
+            let mut sets = game
+                .split(": ")
+                .nth(1)
+                .unwrap()
+                .split(&[';', ' ', ','])
+                .filter(|&x| x != "");
+
+            let mut hm = HashMap::<&str, usize>::new();
+            hm.insert("red", 0);
+            hm.insert("green", 0);
+            hm.insert("blue", 0);
+
+            while let Some(nr) = sets.next() {
+                let (nr, color) = (nr.parse::<usize>().unwrap(), sets.next().unwrap());
+                if nr > hm[color] {
+                    hm.insert(color, nr);
+                }
+            }
+
+            hm["red"] * hm["green"] * hm["blue"]
+        })
+        .sum())
+}
+
 #[test]
 fn solve() {
-    println!("part I : {}\n", possible_games().unwrap(),);
+    println!(
+        "part I : {}\npart II: {}\n",
+        possible_games().unwrap(),
+        min_set_power_sum().unwrap()
+    );
 }
